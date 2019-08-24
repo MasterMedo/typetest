c91ddae9f903224df9193e738f3bdde7a0ec2df8
MasterMedo <mislav.vuletic@gmail.com>
2019-08-25 01:27:29 +0200

    rm: windows

--- typetest/gui.py
+++ typetest/gui.py
@@ -11,18 +11,21 @@ color_correct         = None
 color_correct_reverse = None
 
 stdscr   = None
-testscr  = None
-wordscr  = None
-timescr  = None
-speedscr = None
 
-test_height = 0
+rows_maxy = 0
 test_row_i  = 0
 test_word_i = 0
+test_char_i = 0
+
+prompt = '>>>'
+prompt_size   = 4 # ">>>"
+speed_size    = 7 # "213 wpm"
+time_size     = 8 # "00:00:00"
+word_size     = 0
 
 def init():
-    global stdscr, testscr, wordscr, timescr, speedscr
-    global testscr_height, testscr_rows
+    global word_size
+    global stdscr, rows_maxy, rows
     global color_basic, color_basic_reverse, color_wrong, color_wrong_reverse, color_correct, color_correct_reverse
 
     stdscr = initscr()
@@ -45,126 +48,116 @@ def init():
     color_wrong_reverse   = color_pair(4)
     color_correct         = color_pair(5)
     color_correct_reverse = color_pair(6)
-                                                                # defaults
+
     maxy, maxx = stdscr.getmaxyx()
 
-    testscr_max_height = 3 if typetest.args['--word-by-word'] else maxy
+    rows_maxy = 3 if typetest.args['--word-by-word'] else maxy - int(not typetest.args['--hide'])
 
-    stats_height = int(not typetest.args['--hide'])
-    testscr_height = min(maxy - stats_height, testscr_max_height)
-
-    prompt_size   = 4+1 # ">>> "
-    speedscr_size = 7+1 # "213 wpm"
-    timescr_size  = 8+1 # "00:00:00"
-    wordscr_size  = maxx - speedscr_size - timescr_size - prompt_size
-
-    testscr  = newwin(testscr_height,          maxx,              0,           0)
-    prompt   = newwin(  stats_height,   prompt_size, testscr_height,           0)
-    wordscr  = newwin(  stats_height,  wordscr_size, testscr_height, prompt_size)
-    timescr  = newwin(  stats_height,  timescr_size, testscr_height,
-            maxx - speedscr_size - timescr_size)
-    speedscr = newwin(  stats_height, speedscr_size, testscr_height,
-            maxx - speedscr_size)
-
-    if typetest.args['--word-by-word']:
-        prompt.addstr('>>> ')
-        prompt.refresh()
+    word_size     = maxx - speed_size - time_size - prompt_size - 3
 
     test_text = ' '.join(typetest.test.test_words)
     add_color = lambda char: [char, color_basic]
     text_rows = wrap(test_text, maxx-1, break_long_words=False, break_on_hyphens=False)
-    testscr_rows = [[list(map(add_color, word)) for word in row] for row in map(str.split, text_rows)]
+    rows = [[list(map(add_color, word)) for word in row] for row in map(str.split, text_rows)]
     if typetest.args['--word-by-word']:
-        for i in range(len(testscr_rows[0][0])):
-            testscr_rows[0][0][i][1] = color_basic_reverse
+        for i in range(len(rows[0][0])):
+            rows[0][0][i][1] = color_basic_reverse
     else:
-        testscr_rows[0][0][0][1] = color_basic_reverse
+        rows[0][0][0][1] = color_basic_reverse
 
-def play():
-    while True:
-        draw_testscr()
-        c = testscr.getch()
 
-        c = chr(c) if c != 127 else '\b'
+def draw_stdscr():
+    global stdscr, rows, rows_maxy, test_row_i
 
-        typetest.test.addch(c)
+    stdscr.erase()
 
-        if not typetest.args['--hide']:
-            if typetest.args['--word-by-word']:
-                draw_wordscr()
-            draw_timescr()
-            draw_speedscr()
-
-def draw_testscr():
-    global testscr, testscr_rows, testscr_height
-    global test_row_i, test_word_i
-
-    testscr.erase()
-
-    start_row_i = min(test_row_i, len(testscr_rows) - testscr_height)
-    for row_i, row in enumerate(testscr_rows[start_row_i:start_row_i + testscr_height]):
+    start_row_i = min(test_row_i, len(rows) - rows_maxy)
+    for row_i, row in enumerate(rows[start_row_i:start_row_i + rows_maxy]):
         for word_i, word in enumerate(row):
             for char_i, (char, color) in enumerate(word):
-                testscr.addstr(char, color)
+                stdscr.addstr(char, color)
             if word_i + 1 != len(row):
-                testscr.addstr(' ')
-        if row_i != testscr_height - 1:
-            testscr.addstr('\n')
+                stdscr.addstr(' ')
+        if row_i != rows_maxy - 1:
+            stdscr.addstr('\n')
 
-def draw_wordscr():
-    global testscr_rows, test_row_i, test_word_i
+def draw_stats():
+    word = typetest.test.text_word
 
-    wordscr.erase()
-
-    if typetest.test.triggered_new_word:
-        if typetest.test.last_text_word == typetest.test.last_test_word:
-            for i in range(len(testscr_rows[test_row_i][test_word_i])):
-                testscr_rows[test_row_i][test_word_i][i][1] = color_correct
-
-        else:
-            for i in range(len(testscr_rows[test_row_i][test_word_i])):
-                testscr_rows[test_row_i][test_word_i][i][1] = color_wrong
-
-        test_word_i += 1
-
-        if test_word_i == len(testscr_rows[test_row_i]):
-            test_word_i = 0
-            test_row_i += 1
-
-        for i in range(len(testscr_rows[test_row_i][test_word_i])):
-            testscr_rows[test_row_i][test_word_i][i][1] = color_basic_reverse
-
-    elif typetest.test.text_word != typetest.test.test_word[:len(typetest.test.text_word)]:
-        for i in range(len(testscr_rows[test_row_i][test_word_i])):
-            testscr_rows[test_row_i][test_word_i][i][1] = color_wrong_reverse
-
-    elif typetest.test.text_word == typetest.test.test_word:
-        for i in range(len(testscr_rows[test_row_i][test_word_i])):
-            testscr_rows[test_row_i][test_word_i][i][1] = color_correct_reverse
-
-    elif typetest.test.text_word == typetest.test.test_word[:len(typetest.test.text_word)]:
-        for i in range(len(testscr_rows[test_row_i][test_word_i])):
-            testscr_rows[test_row_i][test_word_i][i][1] = color_basic_reverse
-
-    wordscr.addstr(typetest.test.text_word)
-    wordscr.refresh()
-
-def draw_timescr():
-    global timescr
-    timescr.erase()
     m, s = divmod(int(typetest.test.duration), 60)
     h, m  = divmod(m, 60)
     if h > 23:
         h = 0
-    timescr.addstr(f'{h:02d}:{m:02d}:{s:02d}')
-    timescr.refresh()
+    time = f'{h:02d}:{m:02d}:{s:02d}'
 
-def draw_speedscr():
-    global speedscr
-    speedscr.erase()
     speed = min(int(typetest.test.tmp_speed_wpm), 999)
-    speedscr.addstr(f'{speed} wpm')
-    speedscr.refresh()
+    speed = f'{speed:3d} wpm'
+
+    stdscr.addstr('\n')
+    stdscr.addstr(f'{prompt} {word[-word_size:]:{word_size}s} {time} {speed}')
+    stdscr.refresh()
+    #exit(f'{prompt} {word[-word_size:]:{word_size}s} {time} {speed}')
+
+def change_char_colors():
+    global rows, test_row_i, test_word_i, test_char_i
+
+    if typetest.test.triggered_new_char:
+        if typetest.test.text_raw[-1] == typetest.test.test[typetest.test.text_char_i]:
+            rows[test_row_i][test_word_i][test_char_i][1] = color_correct
+        else:
+            rows[test_row_i][test_word_i][test_char_i][1] = color_wrong
+        test_char_i += 1
+        if test_char_i == len(rows[test_row_i][test_word_i]):
+            test_char_i = 0
+            test_word_i += 1
+            if test_word_i == len(rows[test_row_i]):
+                test_word_i = 0
+                test_row_i += 1
+        rows[test_row_i][test_word_i][test_char_i][1] = color_basic_reverse
+
+    elif typetest.test.triggered_deletion:
+        rows[test_row_i][test_word_i][test_char_i][1] = color_basic
+        test_char_i -= 1
+        if test_char_i < 0:
+            test_word_i -= 1
+            test_char_i = len(rows[test_row_i][test_word_i]) - 1
+            if test_word_i < 0:
+                test_rows_i -= 1
+                test_word_i = len(rows[test_row_i]) - 1
+        rows[test_row_i][test_word_i][test_char_i][1] = color_basic_reverse
+
+def change_word_colors():
+    global rows, test_row_i, test_word_i
+
+    if typetest.test.triggered_new_word:
+        if typetest.test.last_text_word == typetest.test.last_test_word:
+            for i in range(len(rows[test_row_i][test_word_i])):
+                rows[test_row_i][test_word_i][i][1] = color_correct
+
+        else:
+            for i in range(len(rows[test_row_i][test_word_i])):
+                rows[test_row_i][test_word_i][i][1] = color_wrong
+
+        test_word_i += 1
+        if test_word_i == len(rows[test_row_i]):
+            test_word_i = 0
+            test_row_i += 1
+
+        for i in range(len(rows[test_row_i][test_word_i])):
+            rows[test_row_i][test_word_i][i][1] = color_basic_reverse
+
+    elif typetest.test.text_word != typetest.test.test_word[:len(typetest.test.text_word)]:
+        for i in range(len(rows[test_row_i][test_word_i])):
+            rows[test_row_i][test_word_i][i][1] = color_wrong_reverse
+
+    elif typetest.test.text_word == typetest.test.test_word:
+        for i in range(len(rows[test_row_i][test_word_i])):
+            rows[test_row_i][test_word_i][i][1] = color_correct_reverse
+
+    elif typetest.test.text_word == typetest.test.test_word[:len(typetest.test.text_word)]:
+        for i in range(len(rows[test_row_i][test_word_i])):
+            rows[test_row_i][test_word_i][i][1] = color_basic_reverse
+
 
 def shutdown():
     curs_set(1)
