d8405ee1074218b666326535391f09345a1bec05
MasterMedo <mislav.vuletic@gmail.com>
2020-08-12 08:18:28 +0200

    tt.py: added more documentation and renamed `print_line` to `line_height`

--- tt.py
+++ tt.py
@@ -38,10 +38,15 @@ def draw(words, colors, word_i, text, wpm, timestamp):
     """Text wraps the `words` list to the terminal width, and prints
     `NUMBER_OF_ROWS` lines of wrapped words starting with the line
     containing the current word that is being typed.
+    As a consequence this can become slow if the text is really long,
+    and the duration of the test is infinite.
+    A better approach would be to text wrap only `on_resize`, but that
+    would mean there would need to be at least 2 structures holding the
+    same data for `words` and `colors` (one text wrapped, one not).
 
     Already typed words appear in either `color_correct` or `color_wrong`,
-    whereas the currently typed word can additionally be `color_normal`,
-    but appears reversed.
+    whereas the currently typed word can additionally be `color_normal` when
+    the word starts with the typed text but isn't (yet) correct.
     """
     current_word_color = color_correct if words[word_i] == text else \
         color_normal if words[word_i].startswith(text) else \
@@ -51,31 +56,33 @@ def draw(words, colors, word_i, text, wpm, timestamp):
 
     line_i = 0
     len_line = 0
-    print_line = -1
     line_words = []
-    for i, (word, color) in enumerate(zip_longest(words, colors, fillvalue=color_normal)):
-        if print_line >= min(term.height, NUMBER_OF_ROWS):
+    line_height = None
+    for i, (word, color) in enumerate(zip_longest(words, colors,
+                                                  fillvalue=color_normal)):
+        if line_height and line_height >= min(term.height, NUMBER_OF_ROWS):
             break
 
         if len_line + len(word) + len(line_words) > term.width:
-            if print_line >= 0:
+            if line_height is not None:
                 line = ' '.join(line_words)
                 if len_line + len(line_words) - 1 < term.width:
                     line += term.clear_eol
-                echo(term.move_yx(print_line, 0) + line)
-                print_line += 1
+
+                echo(term.move_yx(line_height, 0) + line)
+                line_height += 1
 
             line_i += 1
             len_line = 0
             line_words = []
 
         if i == word_i:
-            print_line += 1
+            line_height = 0
 
-        line_words.append(color + word + term.normal)
+        line_words.append(color + word + color_normal)
         len_line += len(word)
 
-    echo(term.move_yx(print_line, 0))
+    echo(term.move_yx(line_height, 0))
     echo(f">>>{text}{' '*(term.width-len(text)-21)}{wpm:3d} wpm | {timestamp}")
     echo(term.move_x(3 + len(text)))
 
@@ -123,11 +130,12 @@ if __name__ == '__main__':
             elif char == ' ':
                 if text:
                     total_chars += len(word) + 1
-                    color = color_correct if text == word else color_wrong
-                    colors.append(color)
 
                     if text == word:
                         correct_chars += len(word) + 1
+                        colors.append(color_correct)
+                    else:
+                        colors.append(color_wrong)
 
                     wpm = min(int(correct_chars*12/duration), 999)
 
