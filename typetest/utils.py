7229d0d8f56d00db94a465d26075d3c0c47d6089
nzdjb <github@2a1b.com>
2021-10-14 08:45:10 +0000

    style: Appeasing flake8.

--- typetest/utils.py
+++ typetest/utils.py
@@ -1,9 +1,11 @@
 """Various utility functions."""
 
+
 def damerau_levenshtein_distance(word_1: str, word_2: str) -> int:
     """Calculates the distance between two words."""
     inf = len(word_1) + len(word_2)
-    table = [[inf for _ in range(len(word_1) + 2)] for _ in range(len(word_2) + 2)]
+    table = [[inf for _ in range(len(word_1) + 2)] for _ in range(len(word_2)
+                                                                  + 2)]
 
     for i in range(1, len(word_1) + 2):
         table[1][i] = i - 1
@@ -18,7 +20,8 @@ def damerau_levenshtein_distance(word_1: str, word_2: str) -> int:
 
             addition = table[row - 1][col] + 1
             deletion = table[row][col - 1] + 1
-            substitution = table[row - 1][col - 1] + (0 if char_1 == char_2 else 1)
+            substitution = table[row - 1][col - 1] + (0 if char_1 == char_2
+                                                      else 1)
 
             transposition = (
                 table[last_row - 1][last_encountered_col - 1]
@@ -27,7 +30,8 @@ def damerau_levenshtein_distance(word_1: str, word_2: str) -> int:
                 + 1
             )
 
-            table[row][col] = min(addition, deletion, substitution, transposition)
+            table[row][col] = min(addition, deletion, substitution,
+                                  transposition)
 
             if char_1 == char_2:
                 last_row = row
