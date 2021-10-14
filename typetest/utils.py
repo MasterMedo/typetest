04c3067258ada7928f6cbad4a567198062dd2faf
nzdjb <github@2a1b.com>
2021-10-14 06:29:55 +0000

    style: Adjust variable names for style and clarity.

--- typetest/utils.py
+++ typetest/utils.py
@@ -1,23 +1,24 @@
 """Various utility functions."""
 
+def damerau_levenshtein_distance(word_1: str, word_2: str) -> int:
     """Calculates the distance between two words."""
-    inf = len(word1) + len(word2)
-    table = [[inf for _ in range(len(word1) + 2)] for _ in range(len(word2) + 2)]
+    infinity = len(word_1) + len(word_2)
+    table = [[infinity for _ in range(len(word_1) + 2)] for _ in range(len(word_2) + 2)]
 
-    for i in range(1, len(word1) + 2):
+    for i in range(1, len(word_1) + 2):
         table[1][i] = i - 1
-    for i in range(1, len(word2) + 2):
+    for i in range(1, len(word_2) + 2):
         table[i][1] = i - 1
 
-    da = {}
-    for col, c1 in enumerate(word1, 2):
+    last_cols = {}
+    for col, character_1 in enumerate(word_1, 2):
         last_row = 0
-        for row, c2 in enumerate(word2, 2):
-            last_col = da.get(c2, 0)
+        for row, character_2 in enumerate(word_2, 2):
+            last_col = last_cols.get(character_2, 0)
 
             addition = table[row - 1][col] + 1
             deletion = table[row][col - 1] + 1
-            substitution = table[row - 1][col - 1] + (0 if c1 == c2 else 1)
+            substitution = table[row - 1][col - 1] + (0 if character_1 == character_2 else 1)
 
             transposition = (
                 table[last_row - 1][last_col - 1]
@@ -28,8 +29,8 @@
 
             table[row][col] = min(addition, deletion, substitution, transposition)
 
-            if c1 == c2:
+            if character_1 == character_2:
                 last_row = row
-        da[c1] = col
+        last_cols[character_1] = col
 
-    return table[len(word2) + 1][len(word1) + 1]
+    return table[len(word_2) + 1][len(word_1) + 1]
