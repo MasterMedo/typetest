1d4d03a9cf54d2348b5dc20bbd005b9824301791
nzdjb <github@2a1b.com>
2021-10-16 21:09:42 +1300

    style: General linting for utils.py (#18)
    * style: Add module docstring.
    * style: Adjust variable names for style and clarity.
    * style: inf > infinity
    * style: char > character
    * style: Clearer variable names.
    * style: Appeasing flake8.
    * style: python-black fixes.

--- typetest/utils.py
+++ typetest/utils.py
@@ -1,34 +1,37 @@
-def damerau_levenshtein_distance(word1: str, word2: str) -> int:
-    """Calculates the distance between two words."""
-    inf = len(word1) + len(word2)
-    table = [[inf for _ in range(len(word1) + 2)] for _ in range(len(word2) + 2)]
+"""Various utility functions."""
 
-    for i in range(1, len(word1) + 2):
+
+def damerau_levenshtein_distance(word_1: str, word_2: str) -> int:
+    """Calculates the distance between two words."""
+    inf = len(word_1) + len(word_2)
+    table = [[inf for _ in range(len(word_1) + 2)] for _ in range(len(word_2) + 2)]
+
+    for i in range(1, len(word_1) + 2):
         table[1][i] = i - 1
-    for i in range(1, len(word2) + 2):
+    for i in range(1, len(word_2) + 2):
         table[i][1] = i - 1
 
-    da = {}
-    for col, c1 in enumerate(word1, 2):
+    last_encountered_cols = {}
+    for col, char_1 in enumerate(word_1, 2):
         last_row = 0
-        for row, c2 in enumerate(word2, 2):
-            last_col = da.get(c2, 0)
+        for row, char_2 in enumerate(word_2, 2):
+            last_encountered_col = last_encountered_cols.get(char_2, 0)
 
             addition = table[row - 1][col] + 1
             deletion = table[row][col - 1] + 1
-            substitution = table[row - 1][col - 1] + (0 if c1 == c2 else 1)
+            substitution = table[row - 1][col - 1] + (0 if char_1 == char_2 else 1)
 
             transposition = (
-                table[last_row - 1][last_col - 1]
-                + (col - last_col - 1)
+                table[last_row - 1][last_encountered_col - 1]
+                + (col - last_encountered_col - 1)
                 + (row - last_row - 1)
                 + 1
             )
 
             table[row][col] = min(addition, deletion, substitution, transposition)
 
-            if c1 == c2:
+            if char_1 == char_2:
                 last_row = row
-        da[c1] = col
+        last_encountered_cols[char_1] = col
 
-    return table[len(word2) + 1][len(word1) + 1]
+    return table[len(word_2) + 1][len(word_1) + 1]
