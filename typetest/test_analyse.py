9baf19acabebd7f6c986c50276b8b832e4201f27
nzdjb <github@2a1b.com>
2021-10-20 08:31:42 +0000

    style: Formatting.

--- typetest/test_analyse.py
+++ typetest/test_analyse.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 
 from typetest.analyse import plot_word_wpm_distribution
 
+
 class TestAnalyse(unittest.TestCase):
-  def test_plot_word_wpm_distribution(self):
-    pd.read_csv = MagicMock(return_value=pd.DataFrame(
-      columns=['word', 'duration', 'wpm', 'timestamp']
-    ))
-    plt.show = MagicMock()
-    plot_word_wpm_distribution('')
-    pd.read_csv.assert_called_once()
-    plt.show.assert_called_once()
+    def test_plot_word_wpm_distribution(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["word", "duration", "wpm", "timestamp"]
+            )
+        )
+        plt.show = MagicMock()
+        plot_word_wpm_distribution("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
