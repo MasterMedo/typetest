23a8d7ebcc133f2fa5e4b2692afeca87fb9a0dd0
nzdjb <github@2a1b.com>
2021-10-21 08:49:18 +0000

    test: Add basic plot_char_speeds test.

--- test/test_analyse.py
+++ test/test_analyse.py
@@ -1,10 +1,15 @@
+import builtins
 import unittest
 from unittest.mock import MagicMock
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from typetest.analyse import plot_word_wpm_distribution, plot_wpm
+from typetest.analyse import (
+    plot_word_wpm_distribution,
+    plot_wpm,
+    plot_char_speeds,
+)
 
 
 class TestAnalyse(unittest.TestCase):
@@ -66,13 +71,27 @@ class TestAnalyse(unittest.TestCase):
     def test_plot_char_speeds_not_enough_data(self):
         pd.read_csv = MagicMock(
             return_value=pd.DataFrame(
-                columns=["char", "duration", "wpm", "timestamp"]
+                columns=["char", "duration", "wpm", "timestamp"],
             )
         )
         plt.show = MagicMock()
         self.assertRaises(AssertionError, plot_char_speeds, "")
         pd.read_csv.assert_called_once()
         plt.show.assert_not_called()
+
+    def test_plot_char_speeds(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["char", "duration", "wpm", "timestamp"],
+                data=[
+                    ["a", 1, 1000, "2021-10-21T21:21+13:00"]
+                ]
+            )
+        )
+        plt.show = MagicMock()
+        plot_char_speeds("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
 
     def test_plot_word_wpm_distribution(self):
         pd.read_csv = MagicMock(
