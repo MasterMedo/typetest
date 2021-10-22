d34ffaba9618fa85aeb086c0078792b2efb11bb6
nzdjb <github@2a1b.com>
2021-10-22 21:15:21 +1300

    set foundation for automatic pr unit testing (#27)
    * test: Initial test for plot_word_wpm_distribution.
    * style: Formatting.
    * refactor: Move tests to test directory.
    * test: Add `poetry run test` functionality.
    * build: Adjust gha to use new poetry run test.
    * style: Fix style issue.
    * test: Basic tests for plot_wpm.
    * test: Test plot_char_speeds throws assertion when not enough data.
    * test: Add basic plot_char_speeds test.
    * style: Remove unused import.
    * style: Black.
    * test: Add test_plot_n_best_word_speeds.
    * test: Add basic test for plot_mistypes_distribution.

---
+++ test/test_analyse.py
@@ -0,0 +1,140 @@
+import unittest
+from unittest.mock import MagicMock
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from typetest.analyse import (
+    plot_word_wpm_distribution,
+    plot_wpm,
+    plot_char_speeds,
+    plot_n_best_word_speeds,
+    plot_mistypes_distribution,
+)
+
+
+class TestAnalyse(unittest.TestCase):
+    def test_plot_wpm(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=[
+                    "timestamp",
+                    "wpm",
+                    "accuracy",
+                    "actual_duration",
+                    "duration",
+                    "hash",
+                ],
+                data=[
+                    [
+                        "2021-10-21T21:21+13:00",
+                        1000,
+                        100,
+                        5,
+                        5,
+                        "abcda4846a3c2a8469dd77c921ab0b0bcd506b6e9f3",
+                    ],
+                    [
+                        "2021-10-21T21:21+13:00",
+                        1000,
+                        100,
+                        5,
+                        5,
+                        "abcda4846a3c2a8469dd77c921ab0b0bcd506b6e9f3",
+                    ],
+                ],
+            )
+        )
+        plt.show = MagicMock()
+        plot_wpm("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
+
+    def test_plot_wpm_returns_when_not_enough_data(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=[
+                    "timestamp",
+                    "wpm",
+                    "accuracy",
+                    "actual_duration",
+                    "duration",
+                    "hash",
+                ],
+                data=[],
+            )
+        )
+        plt.show = MagicMock()
+        plot_wpm("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_not_called()
+
+    def test_plot_char_speeds_not_enough_data(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["char", "duration", "wpm", "timestamp"],
+            )
+        )
+        plt.show = MagicMock()
+        self.assertRaises(AssertionError, plot_char_speeds, "")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_not_called()
+
+    def test_plot_char_speeds(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["char", "duration", "wpm", "timestamp"],
+                data=[["a", 1, 1000, "2021-10-21T21:21+13:00"]],
+            )
+        )
+        plt.show = MagicMock()
+        plot_char_speeds("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
+
+    def test_plot_n_best_word_speeds(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["word", "duration", "wpm", "timestamp"],
+                data=[
+                    ["turtle", 4, 1001, "2021-10-21T21:21+13:00"],
+                    ["pigeon", 5, 999, "2021-10-21T21:21+13:00"],
+                    ["albatross", 6, 1000, "2021-10-21T21:21+13:00"],
+                    ["giraffe", 6, 1000, "2021-10-21T21:21+13:00"],
+                    ["elephant", 6, 1000, "2021-10-21T21:21+13:00"],
+                ],
+            )
+        )
+        plt.show = MagicMock()
+        plot_n_best_word_speeds("", 2)
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
+
+    def test_plot_word_wpm_distribution(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["word", "duration", "wpm", "timestamp"]
+            )
+        )
+        plt.show = MagicMock()
+        plot_word_wpm_distribution("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
+
+    def test_plot_mistypes_distribution(self):
+        pd.read_csv = MagicMock(
+            return_value=pd.DataFrame(
+                columns=["word", "mistype", "timestamp"],
+                data=[
+                    ["turtle", "trutle", "2021-10-21T21:21+13:00"],
+                    ["pigeon", "pgeone", "2021-10-21T21:21+13:00"],
+                    ["albatross", "abbatros", "2021-10-21T21:21+13:00"],
+                    ["giraffe", "gragfe", "2021-10-21T21:21+13:00"],
+                    ["elephant", "elelgpaglea", "2021-10-21T21:21+13:00"],
+                ],
+            )
+        )
+        plt.show = MagicMock()
+        plot_mistypes_distribution("")
+        pd.read_csv.assert_called_once()
+        plt.show.assert_called_once()
