89650c15cc7235ad1e1e0b9e988d7eacb790f371
MasterMedo <mislav.vuletic@gmail.com>
2021-10-26 18:19:53 +0200

    separate typetest-analyse into multiple modules

---
+++ typetest/analyse/typing_speed_of_n_best_words.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from collections import deque
+
+from typetest.utils import check_files
+
+
+@check_files
+def plot(input_file, n, filter_func=lambda w: True):
+    """Loads all words from `input_file` and groups them by word."""
+
+    data_frame = pd.read_csv(
+        input_file,
+        header=None,
+        names=["word", "duration", "wpm", "timestamp"],
+    )
+
+    grouped_data_frames = list(
+        filter(lambda t: filter_func(t[0]), data_frame.groupby("word"))
+    )
+
+    with open("least_typed_words", "w") as f:
+        words_by_freq = list(
+            zip(*sorted(grouped_data_frames, key=lambda t: len(t[1])))
+        )[0]
+        f.write(" ".join(words_by_freq))
+
+    grouped_data_frames = sorted(
+        grouped_data_frames, key=lambda x: x[1]["wpm"].median()
+    )
+
+    with open("worst_words", "w") as f:
+        words_by_median = list(zip(*grouped_data_frames))[0]
+        f.write(" ".join(words_by_median))
+
+    first_half = deque(maxlen=n // 2)
+    second_half = deque(maxlen=n // 2)
+    for word, df in grouped_data_frames:
+        if len(first_half) < first_half.maxlen:
+            first_half.append((word, df["wpm"], df["wpm"].mean()))
+        else:
+            second_half.append((word, df["wpm"], df["wpm"].mean()))
+
+    words, typing_speeds_in_wpm, means = zip(
+        *list(first_half) + list(second_half)
+    )
+    mean = round(sum(means) / len(means))
+
+    fig, ax = plt.subplots()
+
+    ax.boxplot(typing_speeds_in_wpm, labels=words)
+    ax.axhline(y=mean, color="r", linestyle="-", label=f"mean {mean} wpm")
+
+    ax.set_title(f"worst and best {n // 2} words")
+    ax.set_xlabel("")
+    ax.set_yscale("linear")
+    ax.set_ylabel("typing speed [wpm]")
+    ax.legend()
+
+    ticks = plt.yticks()[0]
+    plt.yticks(np.arange(ticks[0], ticks[-1], 10))
+    plt.xticks(rotation=90)
+
+    plt.show()
---
+++ typetest/analyse/typing_speed_per_test_duration.py
@@ -0,0 +1,90 @@
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from itertools import cycle
+from collections import defaultdict
+
+from typetest.utils import check_files
+
+
+@check_files
+def plot(output):
+    """Reads `output` and plots typing speeds (wpm)
+    categorized by buckets of test duration.
+
+    Bucket labels and their time intervals:
+    short: below 20 seconds
+    medium: 20 to 60 seconds
+    long: 60 seconds to 10 minutes
+    extra long: >10 minutes
+    """
+    data_frame = pd.read_csv(
+        output,
+        header=None,
+        names=[
+            "timestamp",
+            "wpm",
+            "accuracy",
+            "actual_duration",
+            "duration",
+            "hash",
+        ],
+    )
+
+    data_frame.timestamp = pd.to_datetime(data_frame.timestamp)
+
+    gdf = defaultdict(lambda: [[], pd.DataFrame()])
+    for index, row in data_frame.iterrows():
+        actual_duration = row["actual_duration"]
+        key = None
+        if actual_duration < 20:
+            key = "short (< 20s)"
+        elif actual_duration >= 20 and actual_duration < 60:
+            key = "medium (> 20s and < 60s)"
+        elif actual_duration >= 60 and actual_duration < 600:
+            key = "long (> 60s and < 600s)"
+        else:
+            key = "extra long (> 600s)"
+        indexes, hdf = gdf[key]
+        indexes.append(index)
+        hdf = hdf.append(row)
+        gdf[key] = indexes, hdf
+
+    grouped = gdf.items()
+    fig, ax = plt.subplots()
+    colors = cycle(sns.color_palette())
+    for key, (indexes, hdf) in grouped:
+        x = indexes
+        y = hdf.wpm
+        color = next(colors)
+        ax.plot(x, y, color=color, lw=3, label=key)
+        if len(x) > 1:
+            trendline = np.poly1d(np.polyfit(x, y, 1))(x)
+            ax.plot(x, trendline, "-", lw=4, color="white")
+            ax.plot(x, trendline, "--", lw=2, label="trendline", color=color)
+
+    # plot accuracy
+    ax.plot(data_frame.accuracy, color="white", lw=4, alpha=0.5)
+    ax.plot(
+        data_frame.accuracy,
+        color=next(colors),
+        lw=1.5,
+        label="accuracy [%]",
+        alpha=0.5,
+    )
+
+    ax.set_title("typing speed categorized by test duration")
+    ax.set_xlabel("")
+    ax.set_ylabel("typing speed [wpm]")
+    ax.legend()
+
+    # add ticks
+    plt.xticks(data_frame.index, data_frame.timestamp.dt.date, rotation=90)
+    for i, label in enumerate(ax.xaxis.get_ticklabels()):
+        if i % math.ceil(len(data_frame) / 50):
+            label.set_visible(False)
+
+    plt.show()
