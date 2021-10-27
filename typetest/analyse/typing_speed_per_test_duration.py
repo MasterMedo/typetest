e8979dde2d46459e7cfb5e2f6fbaeb6dcd4a48e9
MasterMedo <mislav.vuletic@gmail.com>
2021-10-27 08:08:20 +0200

    improve typing speed per test duration plot

--- typetest/analyse/typing_speed_per_test_duration.py
+++ typetest/analyse/typing_speed_per_test_duration.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 
 from itertools import cycle
 from collections import defaultdict
+from matplotlib.ticker import MaxNLocator, FuncFormatter
 
 from typetest.utils import validate_input_file_path
 
 
 @validate_input_file_path
-def plot(output):
-    """Reads `output` and plots typing speeds (wpm)
+def plot(input_file_path):
+    """Reads `input_file_path` and plots typing speeds (wpm)
     categorized by buckets of test duration.
 
     Bucket labels and their time intervals:
@@ -22,7 +23,7 @@ def plot(output):
     extra long: >10 minutes
     """
     data_frame = pd.read_csv(
-        output,
+        input_file_path,
         header=None,
         names=[
             "timestamp",
@@ -34,39 +35,14 @@ def plot(output):
         ],
     )
 
-    data_frame.timestamp = pd.to_datetime(data_frame.timestamp)
+    data_frame.timestamp = pd.to_datetime(data_frame.timestamp).dt.date
 
-    gdf = defaultdict(lambda: [[], pd.DataFrame()])
-    for index, row in data_frame.iterrows():
-        actual_duration = row["actual_duration"]
-        key = None
-        if actual_duration < 20:
-            key = "short (< 20s)"
-        elif actual_duration >= 20 and actual_duration < 60:
-            key = "medium (> 20s and < 60s)"
-        elif actual_duration >= 60 and actual_duration < 600:
-            key = "long (> 60s and < 600s)"
-        else:
-            key = "extra long (> 600s)"
-        indexes, hdf = gdf[key]
-        indexes.append(index)
-        hdf = hdf.append(row)
-        gdf[key] = indexes, hdf
+    grouped_data_frames = data_frame.groupby(
+        pd.cut(data_frame["actual_duration"], [0, 20, 60, 600])
+    )
 
-    grouped = gdf.items()
     fig, ax = plt.subplots()
     colors = cycle(sns.color_palette())
-    for key, (indexes, hdf) in grouped:
-        x = indexes
-        y = hdf.wpm
-        color = next(colors)
-        ax.plot(x, y, color=color, lw=3, label=key)
-        if len(x) > 1:
-            trendline = np.poly1d(np.polyfit(x, y, 1))(x)
-            ax.plot(x, trendline, "-", lw=4, color="white")
-            ax.plot(x, trendline, "--", lw=2, label="trendline", color=color)
-
-    # plot accuracy
     ax.plot(data_frame.accuracy, color="white", lw=4, alpha=0.5)
     ax.plot(
         data_frame.accuracy,
@@ -76,15 +52,29 @@ def plot(output):
         alpha=0.5,
     )
 
+    for duration, grouped_data_frame in grouped_data_frames:
+        x = grouped_data_frame.index.values.tolist()
+        y = grouped_data_frame.wpm
+        color = next(colors)
+        ax.plot(x, y, color=color, lw=3, label=duration)
+        if len(x) > 1:
+            trendline = np.poly1d(np.polyfit(x, y, 1))(x)
+            ax.plot(x, trendline, "-", lw=4, color="white")
+            ax.plot(x, trendline, "--", lw=2, label="trendline", color=color)
+
     ax.set_title("typing speed categorized by test duration")
-    ax.set_xlabel("")
+    ax.set_xlabel("date of taking the particular test")
     ax.set_ylabel("typing speed [wpm]")
+
+    ax.xaxis.set_major_locator(MaxNLocator(20))
+    ax.xaxis.set_major_formatter(
+        FuncFormatter(
+            lambda index, _: data_frame.iloc[int(index) - 1].timestamp
+            if int(index) < len(data_frame)
+            else ""
+        )
+    )
+    plt.xticks(rotation=90)
+
     ax.legend()
-
-    # add ticks
-    plt.xticks(data_frame.index, data_frame.timestamp.dt.date, rotation=90)
-    for i, label in enumerate(ax.xaxis.get_ticklabels()):
-        if i % math.ceil(len(data_frame) / 50):
-            label.set_visible(False)
-
     plt.show()
